12d1237c1d27b5520981f2e296310f06866a8b4b
Hosein <asadolahi.hosein226@gmail.com>
2025-01-02 20:21:05 +0330

    add login logout csrf cors security

--- src/main/java/com/hosein/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/hosein/jobportal/config/WebSecurityConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     private final String[] publicUrl = {"/", "/global-search/**", "/register", "/register/**",
             "/webjars/**", "/resources/**", "/assets/**", "/css/**", "/summernote/**", "/js/**",
@@ -27,14 +30,24 @@ public class WebSecurityConfig {
             auth.requestMatchers(publicUrl).permitAll();
             auth.anyRequest().authenticated();
         });
+        http.formLogin(formLogin -> {
+            formLogin.loginPage("/login").permitAll().successHandler(customAuthenticationSuccessHandler);
+        }).logout(logout -> {
+            logout.logoutUrl("/logout").logoutSuccessUrl("/").;
+        }).cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable());
         return http.build();
     }
 
     @Bean
     public AuthenticationProvider authProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+        authProvider.setPasswordEncoder(passwordEncoder());
         authProvider.setUserDetailsService(userDetailsService);
         return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/hosein/jobportal/services/UserService.java
+++ src/main/java/com/hosein/jobportal/services/UserService.java
@@ -9,6 +9,7 @@ import com.hosein.jobportal.repository.RecruiterProfileRepository;
 import com.hosein.jobportal.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -20,10 +21,12 @@ public class UserService {
     private final UserRepository userRepository;
     private final JobSeekerProfileRepository jspRepository;
     private final RecruiterProfileRepository rpRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User save(User user) {
         user.setActive(true);
         user.setRegistrationDate(new Date());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         User savedUser = null;
         try {
             savedUser = userRepository.save(user);
